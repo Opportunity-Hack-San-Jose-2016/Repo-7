7422798fabd1c29426a9829ed40b2799240eb6c0
Billy <mr.bill.ly@gmail.com>
2016-07-09 14:56:41 -0700

    created help and coupon fragments

--- UP_Opportunity/app/src/main/java/com/up/up_opportunity/MainActivity.java
+++ UP_Opportunity/app/src/main/java/com/up/up_opportunity/MainActivity.java
@@ -33,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
         bottomBarClickListener(bottomBar);
 
-
     }
+
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
---
+++ UP_Opportunity/app/src/main/java/com/up/up_opportunity/fragments/CouponFragment.java
@@ -0,0 +1,27 @@
+package com.up.up_opportunity.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.up.up_opportunity.R;
+
+/**
+ * Created by Billy on 7/9/16.
+ */
+public class CouponFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_coupon, container, false);
+        setRetainInstance(true);
+
+
+
+        return v;
+    }
+}
