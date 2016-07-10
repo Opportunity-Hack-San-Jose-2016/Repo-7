4779d01a0b1ce442cc68ea850d97a13cad385b32
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-07-10 05:12:16 -0700

    EventsFragment: Added Shared Preferences

--- UP_Opportunity/app/src/main/java/com/up/up_opportunity/fragments/EventsFragment.java
+++ UP_Opportunity/app/src/main/java/com/up/up_opportunity/fragments/EventsFragment.java
@@ -1,5 +1,7 @@
 package com.up.up_opportunity.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.up.up_opportunity.R;
+import com.up.up_opportunity.fragments.jobs.JobsRVAdapter;
 import com.up.up_opportunity.model.event.GoogleEvent;
 import com.up.up_opportunity.model.job.Indeed;
 import com.up.up_opportunity.providers.EventApi;
@@ -43,7 +46,7 @@ public class EventsFragment extends android.support.v4.app.Fragment {
     RecyclerView recyclerView;
     GoogleEventsRecyclerView recyclerViewAdapter;
     private GoogleEvent googleEvent;
-
+    SharedPreferences sharedPreferences;
 
     @Nullable
     @Override
@@ -52,6 +55,19 @@ public class EventsFragment extends android.support.v4.app.Fragment {
         setRetainInstance(true);
         recyclerView = (RecyclerView) v.findViewById(R.id.list_recyclerView_event);
 
+        sharedPreferences = getActivity().getSharedPreferences("EVENTS", Context.MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("Event","");
+        if(json != ""){
+            GoogleEvent googleEvent = gson.fromJson(json, GoogleEvent.class);
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerViewAdapter = new GoogleEventsRecyclerView(googleEvent);
+            recyclerView.setAdapter(recyclerViewAdapter);
+        }else{
+            eventApiCall();
+        }
 
 
         return v;
@@ -67,70 +83,79 @@ public class EventsFragment extends android.support.v4.app.Fragment {
 
             @Override
             public void onClick(View view) {
-
-                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-                OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                        .addInterceptor(logging)
-                        .build();
-
-                GsonBuilder gsonBuilder = new GsonBuilder()
-                        .setLenient();
-                Gson gson = gsonBuilder.create();
-
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/")
-                        .addConverterFactory(GsonConverterFactory.create(gson))
-                        .client(okHttpClient)
-                        .build();
-
-                EventApi service = retrofit.create(EventApi.class);
-
-                //https://maps.googleapis.com/maps/api/place/nearbysearch/json?
-                // location=37.3382,-121.8863&
-                // radius=500&
-                // name=events&
-                // key=AIzaSyA0ls9fcQ0Gnec7lTuV5xmLci3nrOQcePI
-
-
-                String apiKey = "AIzaSyA0ls9fcQ0Gnec7lTuV5xmLci3nrOQcePI";
-                String latLong= "37.3382,-121.8863";
-                String radius = "500";
-                String name = "events";
-
-
-                Call<GoogleEvent> call = service.getGoogleEvents(latLong , radius, name, apiKey);
-                call.enqueue(new Callback<GoogleEvent>() {
-                    @Override
-                    public void onResponse(Call<GoogleEvent> call, Response<GoogleEvent> response) {
-                        if(response.isSuccessful()){
-                            googleEvent = response.body();
-                            Double lat = googleEvent.getResults().get(0).getGeometry().getLocation().getLat();
-                            String name = googleEvent.getResults().get(0).getName();
-                            String vicinity = googleEvent.getResults().get(0).getVicinity();
-                            int size = googleEvent.getResults().size();
-                            Log.d(TAG, "Event Name: " + name);
-                            Log.d(TAG, "Event Name: " + vicinity);
-                            Log.d(TAG, "Event Name: " + lat);
-                            Log.d(TAG, "Size: " + size);
-                            recyclerViewAdapter = new GoogleEventsRecyclerView(googleEvent);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                            recyclerView.setAdapter(recyclerViewAdapter);
-//                            recyclerViewAdapter.notifyDataSetChanged();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<GoogleEvent> call, Throwable t) {
-                        t.printStackTrace();
-                    }
-                });
+            eventApiCall();
 
 
             }
         });
 
+    }
 
+    private void eventApiCall(){
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        GsonBuilder gsonBuilder = new GsonBuilder()
+                .setLenient();
+        Gson gson = gsonBuilder.create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(okHttpClient)
+                .build();
+
+        EventApi service = retrofit.create(EventApi.class);
+
+        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?
+        // location=37.3382,-121.8863&
+        // radius=500&
+        // name=events&
+        // key=AIzaSyA0ls9fcQ0Gnec7lTuV5xmLci3nrOQcePI
+
+
+        String apiKey = "AIzaSyA0ls9fcQ0Gnec7lTuV5xmLci3nrOQcePI";
+        String latLong= "37.3382,-121.8863";
+        String radius = "500";
+        String name = "events";
+
+
+        Call<GoogleEvent> call = service.getGoogleEvents(latLong , radius, name, apiKey);
+        call.enqueue(new Callback<GoogleEvent>() {
+            @Override
+            public void onResponse(Call<GoogleEvent> call, Response<GoogleEvent> response) {
+                if(response.isSuccessful()){
+                    googleEvent = response.body();
+                    Double lat = googleEvent.getResults().get(0).getGeometry().getLocation().getLat();
+                    String name = googleEvent.getResults().get(0).getName();
+                    String vicinity = googleEvent.getResults().get(0).getVicinity();
+                    int size = googleEvent.getResults().size();
+                    Log.d(TAG, "Event Name: " + name);
+                    Log.d(TAG, "Event Name: " + vicinity);
+                    Log.d(TAG, "Event Name: " + lat);
+                    Log.d(TAG, "Size: " + size);
+                    recyclerViewAdapter = new GoogleEventsRecyclerView(googleEvent);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    recyclerView.setAdapter(recyclerViewAdapter);
+
+                    SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+                    Gson gson = new Gson();
+                    String json = gson.toJson(googleEvent);
+                    prefsEditor.putString("Event", json);
+                    prefsEditor.commit();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GoogleEvent> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
 }
