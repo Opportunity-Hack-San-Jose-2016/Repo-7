c6bdee20a70df381bd20a752d7adb55be767d7dc
Billy <mr.bill.ly@gmail.com>
2016-07-09 22:46:32 -0700

    fix gitignore

---
+++ UP_Opportunity/app/src/main/java/com/up/up_opportunity/keys/keys.java
@@ -0,0 +1,10 @@
+package com.up.up_opportunity.keys;
+
+/**
+ * Created by Billy on 7/9/16.
+ */
+public class keys {
+
+    public final static String COUPON_KEY = "360910f6c6c64841cae140dcdfcc9962ffd260d33b7fdde1dddad5ea2cfee4fca6093d039948f5a86182c0c94baacba2";
+
+}
