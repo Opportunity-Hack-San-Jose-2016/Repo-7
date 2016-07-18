4c0107d1ac3b7a9120cd7a538c93195e77441db9
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-07-18 16:59:12 -0700

    Refactor view_holders

--- UP_Opportunity/app/src/main/java/com/up/up_opportunity/view_holders/QuestionViewHolder.java
+++ UP_Opportunity/app/src/main/java/com/up/up_opportunity/view_holders/QuestionViewHolder.java
@@ -6,23 +6,26 @@ import android.widget.TextView;
 
 import com.up.up_opportunity.R;
 import com.up.up_opportunity.model.forum.Question;
-import com.yelp.clientlib.entities.Business;
 
 /**
  * Created by adao1 on 7/9/2016.
  */
 public class QuestionViewHolder extends RecyclerView.ViewHolder{
+
     public TextView questionTV;
     public TextView timeTV;
     public TextView dateTV;
     public TextView numAnswersTV;
+
     public QuestionViewHolder(View itemView) {
         super(itemView);
+
         questionTV = (TextView)itemView.findViewById(R.id.question_text_id);
         timeTV = (TextView)itemView.findViewById(R.id.question_time_id);
         dateTV = (TextView)itemView.findViewById(R.id.question_date_id);
         numAnswersTV = (TextView)itemView.findViewById(R.id.question_numAnswers_id);
     }
+
     public void bind(final Question question, final OnQuestionClickListener listener){
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +37,4 @@ public class QuestionViewHolder extends RecyclerView.ViewHolder{
     public interface OnQuestionClickListener{
         void onQuestionClick(Question question);
     }
-
 }
